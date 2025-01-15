0e3729cdfdd83888c431daee83763edf1633c012
chaithanya <chaitanyaprasad.182@gmail.com>
2025-01-15 04:14:02 +0000

    solution

---
+++ src/components/Gaming/styledComponents.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+import {UlHomeCont} from '../Home/styledComponents'
+
+export const GamingUlCont = styled(UlHomeCont)`
+  justify-content: center;
+  align-items: center;
+`
